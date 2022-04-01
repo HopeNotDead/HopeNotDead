41a56557935459b2f67d31d7e26f2773d35e784e
HopeNotDead <869132116@qq.com>
2022-04-01 12:15:24 +0800

    test

--- assets/About.2a533885.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index.3c81f60f.js";import{d as o,c as a,t as r,o as n}from"./vendor.609315a3.js";const s=o({name:"About",data(){return{msg:"About Page"}},setup(){}});function p(t,c,m,u,f,i){return n(),a("h1",null,r(t.msg),1)}var l=e(s,[["render",p]]);export{l as default};
---
+++ assets/About.3ebe44f6.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.a411a7b8.js";import{d as o,c as a,t as r,o as n}from"./vendor.609315a3.js";const s=o({name:"About",data(){return{msg:"About Page"}},setup(){}});function p(t,c,m,u,f,i){return n(),a("h1",null,r(t.msg),1)}var l=e(s,[["render",p]]);export{l as default};
